58cc69d2db798ed2e7300d242783542ee72d6265
Danko05 <119284201+Danko05@users.noreply.github.com>
2023-04-28 22:42:42 +0300

    Lab5

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,32 @@
+package org.example;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public List<String>  shuttleMissions(String text) {
+        Pattern pattern = Pattern.compile("/shuttle/missions/(\\D{3}-\\d)_(\\d)");
+        Matcher matcher = pattern.matcher(text);
+        List<String> list = new ArrayList<>();
+
+        while (matcher.find()) {
+            list.add(matcher.group() + "\n");
+            list.add("Mission name is: " + matcher.group(1) + "\n");
+            list.add("Order number is: " + matcher.group(2) + "\n" + "\n");
+        }
+        System.out.println(list);
+        return list;
+    }
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        Main main = new Main();
+        main.shuttleMissions(text) ;
+    }
+}
